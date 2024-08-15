8dedcfdac697f9219091e9dc5692b94ce03f77a5
himansumuduli <97511041+himansumuduli@users.noreply.github.com>
2024-08-15 22:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Function to display the length of the current URL
+function showUrlLength() {
+ 
+    let currentUrl = window.location.href;
+    
+
+    let urlLength = currentUrl.length;
+    
+   
+    alert("The length of the URL is: " + urlLength);
+}
+
+// Call the function to show the URL length
+showUrlLength();
